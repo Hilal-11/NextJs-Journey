e0f5ba842c80519ed87132806b1ecd74dbefe9e5
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-12 20:33:40 +0530

    useRouter in nextjs

--- nextjs-fundamentals/src/app/page.tsx
+++ nextjs-fundamentals/src/app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
+
+
 export default function Home() {
+
+  const router = useRouter();
+  console.log(router)
+
   return (
     <div>
       <h1>Home page</h1>
       <div>
-        <Link href={'/Login'}>Go to Login</Link>
-      
+        <Link href={'/Login'}>Go to Login</Link> <br />
+        <button onClick={() => router.push("/Login/loginadmin")} className=" text-center px-5 py-2 rounded-2xl my-10 bg-neutral-900 text-white">Click to navigate</button>
       </div>
     </div>
   );
