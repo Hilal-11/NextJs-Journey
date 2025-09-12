2121fb84b59090bc12a73e233311baa8ed55c181
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-12 20:11:44 +0530

    ...

---
+++ nextjs-fundamentals/src/app/products/page.tsx
@@ -0,0 +1,7 @@
+const Products = () => {
+    return <div>
+        <p className="text-center">You can viwe the products</p>
+    </div>
+}
+
+export default Products;
---
+++ nextjs-fundamentals/src/app/products/specificproduct/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpecificProduct = () => {
+  return (
+    <div>
+      <h1 className='text-center'>Specific Product</h1>
+    </div>
+  )
+}
+
+export default SpecificProduct
