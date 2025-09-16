f0a1d9f4d5d392c5140bfdaa1596d5aaab033e0f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-17 01:58:02 +0530

    aceternity UI

--- shad-cn/src/app/page.tsx
+++ shad-cn/src/app/page.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import Header from '@/components/Header';
 import Theme from '@/components/Theme';
+import { MacbookScroll } from "@/components/ui/macbook-scroll";
 export default function Home() {
   return (
-   <div className="relative container mx-auto flex p-4">
+   <div className="relative container mx-auto flex flex-col p-4">
     <Header />
     <div className=" absolute right-10">
         <Theme />
       </div>
+    <main className="flex flex-col items-center justify-center w-full">
+      <MacbookScroll />
+    </main>
   </div>
   );
 }
